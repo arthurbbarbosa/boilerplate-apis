db7c90424108f2ff9b46d9e09475a0563fcc5003
arthurbbarbosa <atsushithesushi@gmail.com>
2025-04-26 00:12:15 -0300

    RUST: changed actix_web to rocket

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,21 @@
-use actix_web::*;
-use serde::Serialize;
+use rocket::serde::json::Json;
+#[macro_use] extern crate rocket;
 
-#[derive(Serialize)]
+#[derive(serde::Serialize)]
+#[serde(crate = "rocket::serde")]
 struct Index {
   message: String
 }
 
 #[get("/")]
-async fn index() -> Result<impl Responder> {
-  Ok(
-    web::Json(Index {
-      message: "Hello, World".to_string()
-    })
-  )
+fn index() -> Json<Index> {
+  Json(Index {
+    message: "Hello, World!".to_string()
+  })
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| App::new().service(index))
-    .bind("0.0.0.0:3000")?
-    .run()
-    .await
+#[launch]
+fn rocket() -> _ {
+  rocket::build()
+    .mount("/", routes![index])
 }
